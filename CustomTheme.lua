1693f1bc636603f86523a5baed4d9cb20e30a1a2
ECCSco <134647048+ECCSco@users.noreply.github.com>
2024-10-20 00:06:38 +0300

    Update CustomTheme.lua

--- CustomTheme.lua
+++ CustomTheme.lua
@@ -2,7 +2,7 @@ CustomTheme = Instance.new("TextButton")
 CustomThemeImg = Instance.new("ImageLabel")
 CustomThemeUICorner = Instance.new("UICorner")
 CustomThemeUIStroke = Instance.new("UIStroke")
-BlockBackgroundSettings = Instance.new("ImageButton")
+--[[BlockBackgroundSettings = Instance.new("ImageButton")
 BlockBackgroundSettingsUICorner = Instance.new("UICorner")
 BackFromSettingButton = Instance.new("TextButton")
 BackFromSettingButtonUICorner = Instance.new("UICorner")
@@ -15,9 +15,9 @@ BTextBoxUICorner = Instance.new("UICorner")
 SetCustomTheme = Instance.new("TextButton")
 SetCustomThemeUICorner = Instance.new("UICorner")
 CustomThemeName = Instance.new("TextBox")
-CustomThemeNameUICorner = Instance.new("UICorner")
+CustomThemeNameUICorner = Instance.new("UICorner")]]
 
-BlockBackgroundSettings.Parent = SearcherSettingsFrame
+--[[BlockBackgroundSettings.Parent = SearcherSettingsFrame
 BlockBackgroundSettings.Size = UDim2.new(0, 485, 0, 233)
 BlockBackgroundSettings.Position = UDim2.new(0.5, 0, 0.5, 0)
 BlockBackgroundSettings.AnchorPoint = Vector2.new(0.5, 0.5)
@@ -246,4 +246,54 @@ PaidModeButtonPart1.BackgroundColor3 = Color3.fromRGB(RTextBox.Text, GTextBox.Te
 PaidModeUIStroke.Color = Color3.fromRGB(RTextBox.Text, GTextBox.Text, BTextBox.Text)
 end)
 end
+end)]]
+
+
+
+CustomTheme.Parent = ThemeBackground
+CustomTheme.BackgroundTransparency = 1
+CustomTheme.AutoButtonColor = false
+CustomTheme.Text = CustomThemeName.Text
+CustomTheme.Font = Enum.Font.ArimoBold
+CustomTheme.TextColor3 = Color3.fromRGB(255, 255, 255)
+CustomTheme.TextSize = 10
+CustomTheme.TextXAlignment = Enum.TextXAlignment.Center
+CustomTheme.TextYAlignment = Enum.TextYAlignment.Bottom
+
+CustomThemeUICorner.CornerRadius = UDim.new(0, 10)
+CustomThemeUICorner.Parent = CustomTheme
+
+CustomThemeUIStroke.Parent = CustomTheme
+CustomThemeUIStroke.ApplyStrokeMode = Enum.ApplyStrokeMode.Border
+CustomThemeUIStroke.Color = Color3.fromRGB(0, 0, 0)
+CustomThemeUIStroke.Thickness = 1.5
+
+CustomThemeImg.Name = "CustomThemeImg"
+CustomThemeImg.Parent = CustomTheme
+CustomThemeImg.BackgroundTransparency = 1
+CustomThemeImg.AnchorPoint = Vector2.new(0.5, 0.5)
+CustomThemeImg.Position = UDim2.new(0.5, 0, 0.5, 0)
+CustomThemeImg.Size = UDim2.new(0, 50, 0, 50)
+CustomThemeImg.Image = "rbxassetid://87037152011988"
+CustomThemeImg.ImageColor3 = Color3.fromRGB(0, 0, 0)
+
+CustomTheme.MouseButton1Click:Connect(function()
+CloseUIButton.TextColor3 = Color3.fromRGB(RTextBox.Text, GTextBox.Text, BTextBox.Text)
+MainFrameUIStroke.Color = Color3.fromRGB(RTextBox.Text, GTextBox.Text, BTextBox.Text)
+ScriptImg.BackgroundColor3 = Color3.fromRGB(RTextBox.Text, GTextBox.Text, BTextBox.Text)
+InfoBoxDescription.BackgroundColor3 = Color3.fromRGB(RTextBox.Text, GTextBox.Text, BTextBox.Text)
+HintWait.BackgroundColor3 = Color3.fromRGB(RTextBox.Text, GTextBox.Text, BTextBox.Text)
+ECCSSearcherSettingsButton.ImageColor3 = Color3.fromRGB(RTextBox.Text, GTextBox.Text, BTextBox.Text)
+TabsLine.BackgroundColor3 = Color3.fromRGB(RTextBox.Text, GTextBox.Text, BTextBox.Text)
+HomeButton.ImageColor3 = Color3.fromRGB(RTextBox.Text, GTextBox.Text, BTextBox.Text)
+ExecutorMenuButton.ImageColor3 = Color3.fromRGB(RTextBox.Text, GTextBox.Text, BTextBox.Text)
+ScriptsLibraryButton.ImageColor3 = Color3.fromRGB(RTextBox.Text, GTextBox.Text, BTextBox.Text)
+SearcherMenuButton.ImageColor3 = Color3.fromRGB(RTextBox.Text, GTextBox.Text, BTextBox.Text)
+SImg.BackgroundColor3 = Color3.fromRGB(RTextBox.Text, GTextBox.Text, BTextBox.Text)
+SImg_2.BackgroundColor3 = Color3.fromRGB(RTextBox.Text, GTextBox.Text, BTextBox.Text)
+Comment.BackgroundColor3 = Color3.fromRGB(RTextBox.Text, GTextBox.Text, BTextBox.Text)
+ThemesHeadFrame.BackgroundColor3 = Color3.fromRGB(RTextBox.Text, GTextBox.Text, BTextBox.Text)
+PaidModeButtonPart1.BackgroundColor3 = Color3.fromRGB(RTextBox.Text, GTextBox.Text, BTextBox.Text)
+PaidModeUIStroke.Color = Color3.fromRGB(RTextBox.Text, GTextBox.Text, BTextBox.Text)
 end)
+
