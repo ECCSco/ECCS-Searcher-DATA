8a42f68d087a7e54993b0e13b62c62524fbce4fa
ECCSco <134647048+ECCSco@users.noreply.github.com>
2024-10-19 21:57:09 +0300

    Update CustomTheme.lua

--- CustomTheme.lua
+++ CustomTheme.lua
@@ -3,6 +3,8 @@ CustomThemeImg = Instance.new("ImageLabel")
 CustomThemeUICorner = Instance.new("UICorner")
 CustomThemeUIStroke = Instance.new("UIStroke")
 
+function zigzag(X) return math.acos(math.cos(X*math.pi))/math.pi end
+
 CustomTheme.Parent = ThemeBackground
 CustomTheme.BackgroundTransparency = 1
 CustomTheme.AutoButtonColor = false
@@ -18,7 +20,7 @@ CustomThemeUICorner.Parent = CustomTheme
 
 CustomThemeUIStroke.Parent = CustomTheme
 CustomThemeUIStroke.ApplyStrokeMode = Enum.ApplyStrokeMode.Border
-CustomThemeUIStroke.Color = Color3.fromRGB(R, G, B)
+CustomThemeUIStroke.Color = Color3.fromRGB(zigzag(R), zigzag(G), zigzag(B))
 CustomThemeUIStroke.Thickness = 1.5
 
 CustomThemeImg.Name = "CustomThemeImg"
@@ -28,7 +30,7 @@ CustomThemeImg.AnchorPoint = Vector2.new(0.5, 0.5)
 CustomThemeImg.Position = UDim2.new(0.5, 0, 0.5, 0)
 CustomThemeImg.Size = UDim2.new(0, 50, 0, 50)
 CustomThemeImg.Image = "rbxassetid://87037152011988"
-CustomThemeImg.ImageColor3 = Color3.fromRGB(R, G, B)
+CustomThemeImg.ImageColor3 = Color3.fromRGB(zigzag(R), zigzag(G), zigzag(B))
 
 CustomTheme.MouseButton1Click:Connect(function()
 CloseUIButton.TextColor3 = Color3.fromRGB(R, G, B)
