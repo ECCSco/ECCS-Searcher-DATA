abb69614d80a8ac6b43a2aebf3a04117fe2ff8e5
ECCSco <134647048+ECCSco@users.noreply.github.com>
2024-10-19 22:29:04 +0300

    Update CustomTheme.lua

--- CustomTheme.lua
+++ CustomTheme.lua
@@ -18,7 +18,7 @@ CustomThemeUICorner.Parent = CustomTheme
 
 CustomThemeUIStroke.Parent = CustomTheme
 CustomThemeUIStroke.ApplyStrokeMode = Enum.ApplyStrokeMode.Border
-CustomThemeUIStroke.Color = Color3.fromRGB(R, G, B)
+CustomThemeUIStroke.Color = Color3.fromRGB(zigzag(R), zigzag(G), zigzag(B))
 CustomThemeUIStroke.Thickness = 1.5
 
 print(CustomThemeUIStroke.Color)
@@ -30,7 +30,7 @@ CustomThemeImg.AnchorPoint = Vector2.new(0.5, 0.5)
 CustomThemeImg.Position = UDim2.new(0.5, 0, 0.5, 0)
 CustomThemeImg.Size = UDim2.new(0, 50, 0, 50)
 CustomThemeImg.Image = "rbxassetid://87037152011988"
-CustomThemeImg.ImageColor3 = Color3.fromRGB(R, G, B)
+CustomThemeImg.ImageColor3 = Color3.fromRGB(zigzag(R), zigzag(G), zigzag(B))
 
 CustomTheme.MouseButton1Click:Connect(function()
 CloseUIButton.TextColor3 = Color3.fromRGB(zigzag(R), zigzag(G), zigzag(B))
